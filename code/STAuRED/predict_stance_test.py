16b040d68fb790a03d81be1bbcab858e3f59086f
AuRED2024 <aured2024@gmail.com>
2024-05-17 18:34:59 +0300

    Update predict_stance_test.py

--- code/STAuRED/predict_stance_test.py
+++ code/STAuRED/predict_stance_test.py
@@ -6,3 +6,35 @@ python predict_stance.py \
  --data_file processed_AuRED/test_fold${fold}_full.txt \
  --lr ${lr} \
  --out_file CV_runs/MARBERTv2_AuRED_stance/MARBERTv2_AuRED_stance_fold${fold}_${lr}_testSet.txt
+
+fold=2
+lr=2e-05
+python predict_stance.py \
+ --pretrained_model models/MARBERTv2_AuRED_stance_fold${fold}_${lr} \
+ --data_file processed_AuRED/test_fold${fold}_full.txt \
+ --lr ${lr} \
+ --out_file CV_runs/MARBERTv2_AuRED_stance/MARBERTv2_AuRED_stance_fold${fold}_${lr}_testSet.txt
+
+fold=3
+lr=2e-05
+python predict_stance.py \
+ --pretrained_model models/MARBERTv2_AuRED_stance_fold${fold}_${lr} \
+ --data_file processed_AuRED/test_fold${fold}_full.txt \
+ --lr ${lr} \
+ --out_file CV_runs/MARBERTv2_AuRED_stance/MARBERTv2_AuRED_stance_fold${fold}_${lr}_testSet.txt
+
+fold=4
+lr=3e-05
+python predict_stance.py \
+ --pretrained_model models/MARBERTv2_AuRED_stance_fold${fold}_${lr} \
+ --data_file processed_AuRED/test_fold${fold}_full.txt \
+ --lr ${lr} \
+ --out_file CV_runs/MARBERTv2_AuRED_stance/MARBERTv2_AuRED_stance_fold${fold}_${lr}_testSet.txt
+
+fold=5
+lr=2e-05
+python predict_stance.py \
+ --pretrained_model models/MARBERTv2_AuRED_stance_fold${fold}_${lr} \
+ --data_file processed_AuRED/test_fold${fold}_full.txt \
+ --lr ${lr} \
+ --out_file CV_runs/MARBERTv2_AuRED_stance/MARBERTv2_AuRED_stance_fold${fold}_${lr}_testSet.txt
