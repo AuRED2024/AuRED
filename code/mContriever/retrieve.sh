4c97c69a0b75953dd165ab3be711946f55dececb
AuRED2024 <aured2024@gmail.com>
2024-05-17 14:11:46 +0300

    Update retrieve.sh

--- code/mContriever/retrieve.sh
+++ code/mContriever/retrieve.sh
@@ -1,3 +1,3 @@
 python dense_retrieval.py --infile /data/AuRED.json \
- --outfile ./output/mcontriever-top5.txt \
+ --outfile ./output/mcontriever-Top5.txt \
  --model "facebook/mcontriever-msmarco"
