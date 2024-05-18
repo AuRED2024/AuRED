dcd6fed5e3e2f756a1f4c7cb23461412536a2da0
AuRED2024 <aured2024@gmail.com>
2024-05-18 10:02:34 +0300

    Update evaluate.sh

--- code/mContriever/evaluate.sh
+++ code/mContriever/evaluate.sh
@@ -1,1 +1,12 @@
+#Standard IR measures [P@5,R@5,MAP,F@5]
+python pyterrier_scorer.py --predicted mcontriever-Top5.txt \
+       --actual /data/qrels/AuRED/AuRED_qrels_verifiable.txt \
+       --output_file BM25_scores.csv
+
+#FEVER evaluation measures
+python FEVER_scorer.py --predicted mcontriever-Top5.txt \
+  --actual /data/qrels/AuRED/AuRED_qrels.txt
+
+
+
 
